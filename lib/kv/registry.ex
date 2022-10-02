699c34d5bd244279b5c6b1540a3873c19ad02c6f
Le Tan Thang <letanthang@gmail.com>
2022-10-02 15:18:59 +0700

    cache by ets

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -6,38 +6,55 @@ defmodule KV.Registry do
   Starts the registry.
   """
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    # 1. Pass the name to GenServer's init
+    server = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server, opts)
   end
 
   ## Client API
 
   @doc """
-  Starts the registry.
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
   """
   def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+    # GenServer.call(server, {:lookup, name})
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
   end
 
   @doc """
   Ensures there is a bucket associated with the given `name` in `server`.
   """
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
   end
 
   ## Defining GenServer Callbacks
 
   @impl true
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {names, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, state) do
-    {names, _} = state
-    {:reply, Map.fetch(names, name), state}
+  def handle_call({:create, name}, _from, {names, refs}) do
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {name, refs}}
+
+      :error ->
+        {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
+    end
   end
 
   @impl true
@@ -48,7 +65,8 @@ defmodule KV.Registry do
       {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
       ref = Process.monitor(pid)
       refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, pid)
+      # names = Map.put(names, name, pid)
+      :ets.insert(names, {name, pid})
       {:noreply, {names, refs}}
     end
   end
@@ -56,8 +74,11 @@ defmodule KV.Registry do
   @impl true
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
-    {:noreply, {names, refs}}     
+    # names = Map.delete(names, name)
+    require Logger
+    Logger.debug("names in KV.Registry: #{inspect(names)}")
+    :ets.delete(names, name)
+    {:noreply, {names, refs}}
   end
 
   @impl true
@@ -66,5 +87,4 @@ defmodule KV.Registry do
     Logger.debug("Unexpected message in KV.Registry: #{inspect(msg)}")
     {:noreply, state}
   end
-
 end
